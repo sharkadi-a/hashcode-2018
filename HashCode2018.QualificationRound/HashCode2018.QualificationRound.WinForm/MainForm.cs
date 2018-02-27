8da1a53fb74d32208810b27838ea4e2172a9d1fa
p.gorbenko <gorpave@gmail.com>
2018-02-27 23:22:30 +0300

    Каркас форм

---
+++ HashCode2018.QualificationRound/HashCode2018.QualificationRound.WinForm/MainForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HashCode2018.QualificationRound.WinForm
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+		MainManager _manager = new MainManager();
+		private void MainForm_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void BrowseBtn_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
