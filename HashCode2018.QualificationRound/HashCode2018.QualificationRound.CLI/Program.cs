48d765c8118cb3e3c7fe56a3ad82dccb9c9dcee9
Sharkadi Andrey <sharkadi.a@yandex.ru>
2018-02-27 23:48:26 +0300

    Заготовка для отборочного раунда готова

---
+++ HashCode2018.QualificationRound/HashCode2018.QualificationRound.CLI/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HashCode2018.QualificationRound.Core;
+
+namespace HashCode2018.QualificationRound.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("FABULOUS CODERS Qualification Round Problem Solution");
+
+            var files = args
+                .Select(arg => new FileInfo(arg))
+                .Where(fi => fi.Exists)
+                .Select(fi => new InputFile(fi))
+                .ToArray();
+            if (files.Length == 0)
+            {
+                files = InputFile
+                    .GetInputFiles(Helpers.GetWorkingDirectoryInfo().FullName)
+                    .ToArray();
+            }
+
+            Console.WriteLine("Total input files: {0}", files.Length);
+            Console.WriteLine("Starting at: {0}", DateTime.Now);
+
+            var problemSolver = new ProblemSolver();
+            problemSolver.SetLogOutput(Console.WriteLine);
+
+            var sw = Stopwatch.StartNew();
+            foreach (var inputFile in files)
+            {
+                ProcessFile(inputFile, problemSolver);
+            }
+            Console.WriteLine("Total time elapsed: {0}", sw.Elapsed);
+            sw.Stop();
+
+            Console.WriteLine();
+            Console.WriteLine("Hit any key to exit...");
+            Console.Read();
+        }
+
+        static void ProcessFile(InputFile inputFile, IProblemSolver problemSolver)
+        {
+            Console.WriteLine("Solving {0}...", inputFile);
+            var sw = Stopwatch.StartNew();
+            using (var outputFile = problemSolver.Solve(inputFile))
+            {
+                Console.WriteLine("Input file solved and saved into: {0}", outputFile);
+                Console.WriteLine("Time elapsed for this input: {0}", sw.Elapsed);
+            }
+            sw.Stop();
+        }
+    }
+}
---
+++ HashCode2018.QualificationRound/HashCode2018.QualificationRound.Core/InputParser.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashCode2018.QualificationRound.Core
+{
+    public static class InputParser
+    {
+        private static IEnumerable<int> Parse(string inputLine)
+        {
+            return inputLine.Split(' ').Select(int.Parse);
+        }
+
+        public static int[] MapMany(string inputLine)
+        {
+            return Parse(inputLine).ToArray();
+        }
+
+        public static int Map1(string inputLine)
+        {
+            return Parse(inputLine).First();
+        }
+
+        public static Tuple<int, int> Map2(string inputLine)
+        {
+            var parsed = Parse(inputLine).ToArray();
+            return new Tuple<int, int>(parsed[0], parsed[1]);
+        }
+        public static Tuple<int, int, int> Map3(string inputLine)
+        {
+            var parsed = Parse(inputLine).ToArray();
+            return new Tuple<int, int, int>(parsed[0], parsed[1], parsed[2]);
+        }
+
+        public static Tuple<int, int, int, int> Map4(string inputLine)
+        {
+            var parsed = Parse(inputLine).ToArray();
+            return new Tuple<int, int, int, int>(parsed[0], parsed[1], parsed[2], parsed[3]);
+        }
+    }
+}
