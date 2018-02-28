af4f3cbd6f9d26891bdb3bc444e7998b84f9cf19
p.gorbenko <gorpave@gmail.com>
2018-02-28 23:27:34 +0300

    отображение

--- HashCode2018.TestRound/HashCode2018.TestRound.WinForm/Drawing/Drawer.cs
+++ HashCode2018.TestRound/HashCode2018.TestRound.WinForm/Drawing/Drawer.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using HashCode2018.TestRound.NetFrameWork;
 
 namespace HashCode2018.TestRound.WinForm.Drawing
@@ -8,7 +9,7 @@ namespace HashCode2018.TestRound.WinForm.Drawing
 		private readonly Graphics _graphics;
 		private readonly int _width;
 		private readonly int _height;
-
+		private readonly Random _random = new Random();
 		public Drawer(Graphics graphics, int width, int height)
 		{
 			_graphics = graphics;
@@ -21,8 +22,13 @@ namespace HashCode2018.TestRound.WinForm.Drawing
 			var pizza = view.Pizza;
 			var slice = view.NewSlice;
 			var cellSize = _width / pizza.Columns;
-			var Brush = new SolidBrush(Color.Blue);
+			var Brush = new SolidBrush(GetRandomColor());
 			_graphics.FillRectangle(Brush, slice.C0*cellSize, slice.R0*cellSize, slice.C1 * cellSize, slice.R1 * cellSize);
+		}
+
+		private Color GetRandomColor()
+		{
+			return Color.FromArgb(_random.Next(25)*10, _random.Next(25) * 10, _random.Next(25) * 10);
 		}
 	}
 }
