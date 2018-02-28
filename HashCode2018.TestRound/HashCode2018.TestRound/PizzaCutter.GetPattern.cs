5d9323833e709931d1ecbb6c02231c5745b47352
p.gorbenko <gorpave@gmail.com>
2018-02-28 22:04:47 +0300

    GetPattern ready

--- HashCode2018.TestRound/HashCode2018.TestRound/PizzaCutter.GetPattern.cs
+++ HashCode2018.TestRound/HashCode2018.TestRound/PizzaCutter.GetPattern.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 
 namespace HashCode2018.TestRound
@@ -23,11 +25,51 @@ namespace HashCode2018.TestRound
 		    var rectangles = new List<Rectangle>();
 		    foreach (var size in templateSized)
 		    {
-			    
+			    ProccessSize(size, rectangles);
 		    }
 
-		    return rectangles;
 
+
+			return rectangles.OrderBy(Rate);
+
+		}
+
+	    private int Rate(Rectangle rec)
+	    {
+		    var size = rec.CellsOffsets.Length;
+
+		    return Math.Min(size / rec.Width, size / rec.Heigth) + size;
+	    }
+
+	    private static void ProccessSize(int size, List<Rectangle> rectangles)
+	    {
+		    for (var i = 1; i <= size; i++)
+		    {
+			    if (size % i == 0)
+			    {
+				    CreateRectangle(size, rectangles, i);
+			    }
+		    }
+	    }
+
+	    private static void CreateRectangle(int size, List<Rectangle> rectangles, int i)
+	    {
+		    var width = i;
+		    var height = size / width;
+		  
+			var cells = new List<CellOffset>();
+		    for (var x = 1; x <= width; x++)
+		    {
+				for (var y = 1; x <= height; x++)
+				{
+					var newCell = new CellOffset(x, y);
+					cells.Add(newCell);
+				}
+			}
+
+		    var readyCells = cells.OrderBy(x => x.x + x.y).ToArray();
+			var newRectangle = new Rectangle(height,width, readyCells);
+			rectangles.Add(newRectangle);
 	    }
     }
 }
