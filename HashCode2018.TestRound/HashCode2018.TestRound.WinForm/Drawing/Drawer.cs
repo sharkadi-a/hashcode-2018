4231962e703906dbf5b03d1c296e1f0a2259c4e4
p.gorbenko <gorpave@gmail.com>
2018-02-28 23:45:02 +0300

    drawer

--- HashCode2018.TestRound/HashCode2018.TestRound.WinForm/Drawing/Drawer.cs
+++ HashCode2018.TestRound/HashCode2018.TestRound.WinForm/Drawing/Drawer.cs
@@ -22,6 +22,7 @@ namespace HashCode2018.TestRound.WinForm.Drawing
 			var pizza = view.Pizza;
 			var slice = view.NewSlice;
 			var cellSize = _width / pizza.Columns;
+			//writeMessage($"{slice.C0}:{slice.R0} {slice.C1}:{slice.R1}");
 			_random = new Random(slice.C0 + slice.C1 + slice.R0 + slice.R1);
 			var color = GetRandomColor();
 			//writeMessage($"{color.R} {color.G} {color.B}");
