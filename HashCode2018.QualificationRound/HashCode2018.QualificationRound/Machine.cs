c66385a0aa161ce1dcfb831c2e37ce4dacc5c366
p.gorbenko <gorpave@gmail.com>
2018-03-01 21:37:00 +0300

    machine

---
+++ HashCode2018.QualificationRound/HashCode2018.QualificationRound/Machine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashCode2018.QualificationRound
+{
+	class Machine
+	{
+		public Point currentPos;
+		public bool IsBusy;
+		public Ride CurrentRide;
+	}
+}
