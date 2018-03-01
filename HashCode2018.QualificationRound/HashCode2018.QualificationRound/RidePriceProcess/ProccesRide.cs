821e94813f8a4fc71243593c22a7ed70f485a787
p.gorbenko <gorpave@gmail.com>
2018-03-01 22:09:41 +0300

    ridePrice

---
+++ HashCode2018.QualificationRound/HashCode2018.QualificationRound/RidePriceProcess/ProccesRide.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashCode2018.QualificationRound.RidePriceProcess
+{
+	class ProccessorPriceRide
+	{
+
+		public int Proccess(Ride ride, int currentTime, int currentPos, out bool failed)
+		{
+
+		}
+	}
+}
