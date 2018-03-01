611880f558a2f7b8c71e5ee6d6612b1125c70a34
p.gorbenko <gorpave@gmail.com>
2018-03-02 00:24:19 +0300

    ++

--- HashCode2018.QualificationRound/HashCode2018.QualificationRound/RidePriceProcess/ProccesRide.cs
+++ HashCode2018.QualificationRound/HashCode2018.QualificationRound/RidePriceProcess/ProccesRide.cs
@@ -27,14 +27,14 @@ namespace HashCode2018.QualificationRound.RidePriceProcess
 			var timeIdle = ride.earlistStart - timeStartReached;
 			if (timeIdle < 0)
 			{
-				timeIdle = 0;
+				timeIdle = timeIdle/2;
 			}
 			var bonus = 0;
 			if (timeStartReached <= ride.earlistStart)
 			{
 				bonus = modelBonus;
 			}
-			var price = timeIdle + timeToStartGo - bonus * 1.001;
+			var price = timeIdle + timeToStartGo - bonus * 0.99;
 			failed = false;
 			return price;
 		}
