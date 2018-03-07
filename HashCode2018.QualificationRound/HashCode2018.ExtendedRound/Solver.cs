53472534dc668f3a8c7aacdf697c476c9ee90049
Михаил Сердюк <mikezars830@gmail.com>
2018-03-07 20:09:57 +0300

    ExtendedRound

---
+++ HashCode2018.QualificationRound/HashCode2018.ExtendedRound/Solver.cs
@@ -0,0 +1,28 @@
+﻿using HashCode2018.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HashCode2018.ExtendedRound
+{
+    public class Solver : IProblemSolver
+    {
+        public void SetIterationCallback(Action<object> callback)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetLogOutput(Action<string> writeLogAction)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OutputFile Solve(InputFile inputFile, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
